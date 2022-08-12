d4af3da8c6bf95e4feebd6ebedbd4ac1ba9e4c4b
Alice3344 <96055666+Alice3344@users.noreply.github.com>
2022-08-12 16:54:53 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function reverse (n) {
-  
+
+  if (n < 0) {
+
+  n = n * -1;
+
+}
+
+  n = n.toString();
+
+  let i = 0;
+
+  let res = '';
+
+  while (i < n.length) {
+
+    res = `${n[i]}${res}`;
+
+    i = i + 1;
+
+  }
+
+  return Number(res);
+
 }
